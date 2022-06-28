525c63f08ac4575ad5799303cbb6384ec4afce5f
HP <HP@LAPTOP-HC2PDFFD.lan>
2022-06-28 16:09:34 +0200

    Versione definitiva

--- src/main/java/it/polito/tdp/alien/FXMLController.java
+++ src/main/java/it/polito/tdp/alien/FXMLController.java
@@ -1,23 +1,93 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.alien;
 
 import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
+	
+	LinkedHashMap<String, LinkedList<String>> listaParole = new LinkedHashMap<String, LinkedList<String>>();
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+	
+	@Override
+	public boolean equals (Object obj) {
+		return super.equals(obj);
+	}
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private TextArea txtMessaggio;
+
+    @FXML
+    private TextField txtTesto;
+
+    @FXML
+    void onHandleClear(ActionEvent event) {
+    	txtMessaggio.clear();
+    	txtTesto.clear();
+    }
+
+    @FXML
+    void onHandleTranslate(ActionEvent event) {
+    	
+    	String parole = txtTesto.getText();
+    	parole = parole.toLowerCase();
+    	String coppia[] = parole.split(" ");
+    	
+    	if (coppia[0].contains("?")) {
+    		String aliena = coppia[0].replace('?', '.');
+    		for (String str : listaParole.keySet()) {
+    			if (str.matches(aliena)) {
+    				coppia[0] = str;
+    				break;
+    			}
+    		}
+    	
+    	parole = coppia[0];
+    	}
+    
+    if ((parole.matches("[a-zA-Z ]+"))) {
+    	if (coppia.length == 2) {
+    		if(!listaParole.containsKey(coppia[0])) {
+    			listaParole.put(coppia[0], new LinkedList<String>());
+    			listaParole.get(coppia[0]).add(coppia[1]);
+    			txtMessaggio.setText("Aggiunta la parola: " + coppia[0] + " e la traduzione: " + coppia [1]);
+    		} else {
+    			listaParole.get(coppia[0]).add(coppia[1]);
+    			txtMessaggio.setText("Aggiunta la parola: " + coppia[0] + " e la traduzione: " + coppia [1]);
+    		}
+    	} else if (coppia.length == 1) {
+    		LinkedList<String> traduzioni = listaParole.get(coppia[0]);
+    		txtMessaggio.setText("La possibile traduzione di " + coppia[0] + " è: \n");
+    		for (String str: traduzioni) {
+    			txtMessaggio.appendText(str + "\n");
+    		}
+    		
+    	}
+    } else {
+    	txtMessaggio.appendText("Sono ammesse solo lettere dell'alfabeto");
+    }
+    	
+    }
+
+    @FXML
     void initialize() {
+        assert txtMessaggio != null : "fx:id=\"txtMessaggio\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 
